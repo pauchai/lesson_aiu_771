728d1b5c33fe9b9524a9e340a3e680e0a31f4c92
pauchai <paul.khimyack@gmail.com>
2024-02-03 07:30:55 +0000

    modify app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ if not image_file is None:                       # Выполнение блок
     col1, col2 = st.columns(2)                   # Создание 2 колонок # st.beta_columns(2)
     image = Image.open(image_file)               # Открытие изображения
     results = process(image_file)                # Обработка изображения с помощью функции, реализованной в другом файле
-    col1.text('Source image')
-    col1.image(image)                       # Вывод в первой колонке уменьшенного исходного изображения
+    st.text("Source image:")
+    st.image(image)                       # Вывод в первой колонке уменьшенного исходного изображения
+    st.text("Результат:")
     st.text(results)                         # Вывод исходного изображения с наложенной маской (по центру)
+    
